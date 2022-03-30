18c74b1836839a3c24495d5fbc3ef672279d2cb4
Louis Law <louiscklaw@users.noreply.github.com>
2022-03-30 20:15:35 +0800

    update,

---
+++ examples/table/task.ps1
@@ -0,0 +1,23 @@
+pushd advancedTable   
+  yarn --dev
+popd
+
+pushd tableFilter     
+  yarn --dev
+popd
+
+pushd useDeleteMany   
+  yarn --dev
+popd
+
+pushd useEditableTable
+  yarn --dev
+popd
+
+pushd useTable        
+  yarn --dev
+popd
+
+pushd useUpdateMany   
+  yarn --dev
+popd
