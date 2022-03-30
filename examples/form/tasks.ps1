4693269bbb23388587d93a0a1fe52797f1e9baf5
Louis Law <louiscklaw@users.noreply.github.com>
2022-03-30 23:17:01 +0800

    update,

---
+++ examples/form/tasks.ps1
@@ -0,0 +1,20 @@
+pushd customValidation
+  scripts/dev.ps1
+popd
+
+pushd useDrawerForm
+  scripts/dev.ps1
+popd
+
+pushd useForm
+  scripts/dev.ps1
+popd
+
+pushd useModalForm
+  scripts/dev.ps1
+popd
+
+pushd useStepsForm
+  scripts/dev.ps1
+popd
+
