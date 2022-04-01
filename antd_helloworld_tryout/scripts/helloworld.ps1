de9e6ddd30485254364854eb5d85e503b268edcc
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-01 13:54:44 +0800

    update,

---
+++ antd_helloworld_tryout/scripts/helloworld.ps1
@@ -0,0 +1,5 @@
+npx create-react-app tutorial --template typescript
+
+pushd tutorial
+  yarn add  @pankod/refine-core @pankod/refine-antd @pankod/refine-react-router-v6
+  yarn add @pankod/refine-simple-rest
