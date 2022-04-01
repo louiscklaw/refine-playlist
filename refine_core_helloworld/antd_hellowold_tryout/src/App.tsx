ba35b78dc77f67403897d8a0e0c0d424ab077ebf
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-01 15:58:56 +0800

    update refine_core_helloworld\antd_hellowold_tryout,

--- refine_core_helloworld/antd_hellowold_tryout/src/App.tsx
+++ refine_core_helloworld/antd_hellowold_tryout/src/App.tsx
@@ -1,11 +1,33 @@
 import React, { FC } from "react";
 import { Button } from "antd";
+
+import { Refine } from "@pankod/refine-core";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+
+import { PostList, PostShow, PostEdit, PostCreate } from "pages/posts";
+import { Layout } from "components/Layout";
+
 import "./App.less";
 
-const App: FC = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+export const App: React.FC = () => {
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      resources={[
+        {
+          name: "posts",
+          // icon: PostIcon,
+          list: PostList,
+          show: PostShow,
+          edit: PostEdit,
+          create: PostCreate,
+        },
+      ]}
+      Layout={Layout}
+    />
+  );
+};
 
 export default App;
