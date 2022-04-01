9ac0c6d3f5b5be4b7c162f59b76b435222ac8fe9
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-01 13:49:50 +0800

    update refine_antd_helloworld,

---
+++ antd_helloworld_tryout/refine_antd_helloworld/scripts/gitUpdate.ps1
@@ -0,0 +1,4 @@
+git add . 
+git commit -m"update refine_antd_helloworld,"
+
+start git push
---
+++ antd_helloworld_tryout/refine_antd_helloworld/src/App.tsx
@@ -0,0 +1,38 @@
+import { Refine } from "@pankod/refine-core";
+import {
+  Layout,
+  ReadyPage,
+  notificationProvider,
+  ErrorComponent,
+} from "@pankod/refine-antd";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+
+import "@pankod/refine-antd/dist/styles.min.css";
+
+import { PostList, PostShow, PostEdit, PostCreate } from "./pages/posts";
+
+export const App: React.FC = () => {
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      Layout={Layout}
+      ReadyPage={ReadyPage}
+      notificationProvider={notificationProvider}
+      catchAll={<ErrorComponent />}
+      resources={[
+        {
+          name: "posts",
+          list: PostList,
+          show: PostShow,
+          edit: PostEdit,
+          create: PostCreate,
+          canDelete: true,
+        },
+      ]}
+    />
+  );
+};
+
+export default App;
