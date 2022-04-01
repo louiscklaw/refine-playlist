5b532fd966b2b0e545e72efd03750bcc31a48062
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-01 15:34:43 +0800

    update helloworld_tryout,

---
+++ refine_core_helloworld/helloworld_tryout/src/App.tsx
@@ -0,0 +1,22 @@
+import { Refine } from "@pankod/refine-core";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+
+// import { PostIcon } from "icons";
+
+export const App: React.FC = () => {
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      resources={[
+        {
+          name: "posts",
+          // icon: PostIcon
+        },
+      ]}
+    />
+  );
+};
+
+export default App;
